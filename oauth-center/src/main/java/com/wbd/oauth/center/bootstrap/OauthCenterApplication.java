426b2f19ffec44013d27ddd17f9e700ee61b155a
zgh <zgh@zgh-PC>
2019-07-18 00:23:45 +0800

    c

---
+++ oauth-center/src/main/java/com/wbd/oauth/center/bootstrap/OauthCenterApplication.java
@@ -0,0 +1,17 @@
+package com.wbd.oauth.center.bootstrap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+@EnableFeignClients //开启feign功能
+@EnableEurekaClient  //开启eureka-client
+@SpringBootApplication
+public class OauthCenterApplication {
+	
+	public static void main(String[] args) {
+		
+	SpringApplication.run(OauthCenterApplication.class, args);
+	}
+
+}
